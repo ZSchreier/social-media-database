aa8e1226543460da963107aef2b126cc01c90795
zschreier <zjschreier@gmail.com>
2023-12-02 20:12:59 -0600

    updated models with proper (and hopefully functioning) parameters

--- models/Thought.js
+++ models/Thought.js
@@ -1,3 +1,71 @@
 // thought model here
 
 // reaction will be a subdocument here
+
+const mongoose = require('mongoose');
+
+const thoughtSchema = new mongoose.Schema(
+  {
+    thoughtText: {
+      type: String,
+      unique: true,
+      required: [true, 'thoughttext required'],
+      maxLength: [280, 'too many characters']
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: formatThoughtCreatedAt
+    },
+    username: {
+      type: String,
+      required: [true, 'username required for thought']
+    },
+    reactions: {
+      type: [reactionSchema]
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const reactionSchema = new mongoose.Schema({
+    reactionId: {
+      type: mongoose.Types.ObjectId,
+      default: new mongoose.Types.ObjectId
+    },
+    reactionBody: {
+      type: String,
+      required: [true, 'reaction body needed'],
+      maxLength: [280, 'max 280 characters please']
+    },
+    username: {
+      type: String,
+      required: [true, 'username required for thought']
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: formatReactionCreatedAt
+    }
+  }
+)
+
+function formatThoughtCreatedAt(createdAt){
+  const formatTime = createdAt.toUTCString()
+  return formatTime
+}
+
+function formatReactionCreatedAt(createdAt){
+  const formatTime = createdAt.toUTCString()
+  return formatTime
+}
+
+thoughtSchema.virtual('friendCount').get(function() {
+  return this.friends.length
+})
+
+const Thought = mongoose.model('thought', thoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,1 +1,39 @@
 // user model here
+
+const {model, Schema} = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: [true, 'username required'],
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, 'email required'],
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'invalid email address']
+    },
+    thoughts: {
+      type: [Schema.Types.ObjectId],
+      ref: 'Thought'
+    },
+    friends: {
+      type: [Schema.Types.ObjectId],
+      ref: 'User'
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length
+})
+
+const User = model('User', userSchema);
+
+module.exports = User;
